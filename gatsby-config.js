0168f9edac72643d8ace927002cf4f93e7830743
Cristina Lugo <cmlugoce@syr.edu>
2019-02-11 11:45:53 -0500

    update description and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Code and Dogs🐕💻 `,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Practicing gatsbyjs by building a simple blog!`,
+    author: `Cristina`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
